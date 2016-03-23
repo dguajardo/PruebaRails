7da9a8552f6a7eeb896ff7daf97bf060dd889062
Diego Guajardo <diego.guajardo@hotmail.cl>
2016-03-22 21:01:53 -0300

    Batman vs sUPERMAN page, two photos, setting it as root page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,12 @@ class PagesController < ApplicationController
   def batman
   	render layout: "landing"
   end
+def superman
+	render layout: "landing"
+end
 
+def bataman_vs_superman
+	render layout: "landing"
+end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
  
-  root 'pages#batman'
+  root 'pages#batman_vs_superman'
 
   get 'pages/batman'
 
   get 'pages/superman'
-
+ 
+  get 'pages/batman_vs_superman'
 
 end
